e95ddfd092d37556d92ceee8d00d6c80d5cea77e
soumyajitz <2014soumyajit@gmail.com>
2017-08-13 18:10:00 -0500

    feat(serialize): passport serialize and deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        })
+});
 
 passport.use(new GoogleStrategy(
     {
@@ -15,10 +25,12 @@ passport.use(new GoogleStrategy(
             User.findOne({ googleId: profile.id })
                 .then((existingUser)=> {
                     if(existingUser) {
-                        console.log("User exists");
+                        done(null, existingUser);
                     }
                     else {
-                        new User({ googleId: profile.id }).save();
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then((newUser) => done(null, newUser))
                     }
                 })
     })
